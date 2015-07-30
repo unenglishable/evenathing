d0ad50368518598040ea5048f503ff30de2becc9
unenglishable <unenglishable@gmail.com>
2015-07-29 17:06:34 -1000

    improve transparency on configuration

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ var routes = require('./routes');
 var serverConfig = require('./config').server;
 
 var server = module.exports = new Hapi.Server();
-server.connection(config.host);
+server.connection({
+  host: config.server.host,
+  port: config.server.port
+});
 server.routes(routes);
