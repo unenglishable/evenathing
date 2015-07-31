7e618525083281968f6341dd29d5f300a2089e3a
unenglishable <unenglishable@gmail.com>
2015-07-30 14:15:45 -1000

    server uses async waterfall to setup

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var Hapi = require('hapi');
-var routes = require('./routes');
-var config = require('./config');
+var async = require('async');
+var path = require('path');
+var routes = require(path.join(__dirname, 'routes'));
+var config = require(path.join(__dirname, 'config'));
 
-var server = module.exports = new Hapi.Server();
-server.connection({
-  host: config.server.host,
-  port: config.server.port
-});
-server.routes(routes);
+module.exports = function(callback) {
+  async.waterfall([
+    function(waterfallCb) {
+      routes.build(function(err, routes) {
+        console.log(routes);
+        return waterfallCb(err, new Hapi.Server(), routes);
+      });
+    },
+    function(server, routes, waterfallCb) {
+      server.connection({
+        host: config.server.host,
+        port: config.server.port
+      });
+      server.route(routes);
+      server.start();
+      return waterfallCb(null, server);
+    }
+  ],
+  function(err, server) {
+    callback(err, server);
+  });
+}
